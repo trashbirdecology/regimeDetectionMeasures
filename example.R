f15f2a0cbba5ee8b9260a0bd6ea9122ff961f0d3
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2018-12-04 12:42:21 -0600

    Update example.R

--- example.R
+++ example.R
@@ -1,5 +1,3 @@
-rm(list= ls())
-
 
 # Munge original data -----------------------------------------------------
 
@@ -7,9 +5,15 @@ origData = munge_orig_dat()
 
 # Visualize original data -------------------------------------------------
 
-plot_orig_data(data = origData, example = F, print = T)
-plot_timeDiff(data = origData, example = F, print = T)
-plot_richness(data = origData, example = F, print = T)
+plot_orig_data(data = origData,
+               example = F,
+               print = T)
+plot_timeDiff(data = origData,
+              example = F,
+              print = T)
+plot_richness(data = origData,
+              example = F,
+              print = T)
 
 # Calculate distance travelled and derviatves of this ---------------------
 
@@ -19,8 +23,15 @@ distances = calculate_distanceTravelled(origData, derivs = T)
 # Calculate FI, VI, and early warning signals --------------------------------------------------------
 
 # Uses a moving window analysis to calculate FI and Vi within each window
-results = rdm_window_analysis(origData, winMove = 0.25, overrideSiteErr = F, min.window.dat = 2, fi.equation = "7.12", to.calc = 'VI')
+results <-
+    rdm_window_analysis(
+        origData,
+        winMove = 0.25,
+        overrideSiteErr = F,
+        min.window.dat = 2,
+        fi.equation = "7.12",
+        to.calc = 'VI'
+    )
 
 
-# END RUN -----------------------------------------------------------------
-
+# END RUN --------------------------------------------------------
