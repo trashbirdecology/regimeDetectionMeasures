ffe8fb483e02a23e8467e7d36778502813160e5c
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2018-12-04 11:50:23 -0600

    Add files via upload

---
+++ example.R
@@ -0,0 +1,30 @@
+rm(list= ls())
+
+
+# Munge original data -----------------------------------------------------
+
+origData = munge_orig_dat()
+
+# Visualize original data -------------------------------------------------
+
+plot_orig_data(data = origData, example = F, print = T)
+plot_timeDiff(data = origData, example = F, print = T)
+plot_richness(data = origData, example = F, print = T)
+
+# Calculate distance travelled and derviatves of this ---------------------
+
+distances = calculate_distanceTravelled(origData, derivs = T)
+
+df2 = merge(distances, origData)
+
+
+# calculate FI, VI, and early warning signals --------------------------------------------------------
+
+# Uses a moving window analysis to calculate FI and Vi within each window
+results = rdm_window_analysis(origData, winMove = 0.25, overrideSiteErr = F, min.window.dat = 2, fi.equation = "7.12")
+
+
+
+
+# END RUN -----------------------------------------------------------------
+
