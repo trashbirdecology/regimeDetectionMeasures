a63df2b12e0d47bcb17072693167826914e0755e
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2018-12-18 10:32:32 -0600

    Update CITATION.R

--- CITATION.R
+++ CITATION.R
@@ -1,11 +1,11 @@
-# year <- sub("-.*", "", 2018)
-# note <- sprintf("R package version %s", "0.0.1")
+ year <- sub("-.*", "", 2018)
+ note <- sprintf("R package version %s", "0.0.1")
 
-# bibentry(bibtype = "Manual",
-#         title = "{regimeDetectionMethods}: Calculating Regime Detection Measures",
-#          author = c(person("Jessica", "Burnett"),
-#                     person("Nathaniel", "Price")
-#                     ),
-#          year = year,
-#          note = note,
-#          url = "https://github.com/TrashBirdEcology/regimeDetectionMeasures")
+ bibentry(bibtype = "Manual",
+         title = "{regimeDetectionMethods}: Calculating Regime Detection Measures",
+          author = c(person("Jessica", "Burnett"),
+                     person("Nathaniel", "Price")
+                     ),
+          year = year,
+          note = note,
+          url = "https://github.com/TrashBirdEcology/regimeDetectionMeasures")
