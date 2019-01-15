4ff0369a243d462b970eb19f1e998102c922cfe2
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-01-15 14:35:42 -0600

    Add dplyr lines to account for duplicate entries

--- R/calculate_varianceIndex.R
+++ R/calculate_varianceIndex.R
@@ -5,19 +5,25 @@
 #' @references [1] Brock and Carpenter YYYY
 #' @export
 calculate_VI <- function(winData, fill = 0) {
-
-    ts <-
-        winData %>%
-        select(-site) %>%
-        # gather(variable, value, -time) %>%
-        group_by(variable) %>%
-        mutate(sumValue = sum(value)) %>%
-        filter(sumValue > 0) %>%
-        select(-sumValue) %>%
-        # unique() %>%
-        spread(variable, value, fill = fill) %>%
-        select(-time) %>%
-        as.matrix()
+ 
+  # Remove site variable if present
+  if("site" %in% colnames(winData)){
+    winData <- winData %>% dplyr::select(-site)
+      }
+# Create the time series to analyse    
+   ts <-
+    winData %>%
+    distinct() %>% 
+    group_by(variable) %>%
+    mutate(sumValue = sum(value)) %>%
+    filter(sumValue > 0) %>%
+    select(-sumValue) %>%
+    group_by(variable, time) %>% 
+    summarise(value = max(value)) %>%
+    ungroup() %>% 
+    spread(variable, value, fill = fill) %>%
+    select(-time) %>%
+    as.matrix()
 
 
     eigCov <- eigen(cov(ts))
