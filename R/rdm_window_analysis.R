991beae01bd3210b18fbf35612bf5d883cd747fb
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-01-16 20:31:34 -0600

    Remove RTENO Crap

--- R/rdm_window_analysis.R
+++ R/rdm_window_analysis.R
@@ -97,16 +97,12 @@ rdm_window_analysis <- function(dataIn,
     resultsOut$FI <- FI %>%  mutate(variable = 'NA')
   }
   
-if (!is.null(EWS) & !("RTENO" %in% colnames(EWS))) {
+if (!is.null(EWS)) {
     resultsOut$EWSs <- EWS %>% tidyr::gather(key = "metricType", 
                                            value = "value", -site, -variable, -winStart, -winStop)
 }
 
-if (!is.null(EWS) & ("RTENO" %in% colnames(EWS))) {
-  resultsOut$EWSs <- EWS %>% tidyr::gather(key = "metricType", 
-                                           value = "value", -site, -variable, -winStart, -winStop, -RTENO) %>% 
-    dplyr::select(-RTENO, everything())
-}
+
   resultsOut <- do.call(plyr::rbind.fill, lapply(resultsOut, data.frame, stringsAsFactors=FALSE)) %>% 
     dplyr::rename(metricValue = value)
   
