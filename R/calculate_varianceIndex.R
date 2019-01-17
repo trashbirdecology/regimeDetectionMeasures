d938e7a923e460b11347b5b6aeb4527e8e53f98e
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-01-16 19:42:54 -0600

    Update calculate_varianceIndex.R

--- R/calculate_varianceIndex.R
+++ R/calculate_varianceIndex.R
@@ -14,6 +14,14 @@ calculate_VI <- function(winData, fill = 0) {
   if(!exists("fill")){
   fill = 0
 }
+  
+  # Add filter for working with BBS data. 
+  if("RTENO" %in% colnames(winData)){
+  winData <- winData %>% dplyr::select(-RTENO)
+  }
+  
+  
+  
 # Create the time series to analyse
    ts <-
     winData %>%
