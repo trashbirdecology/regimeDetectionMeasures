dc55dd152e0b86f0af2869862d6cfff4cde4dc04
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-01-16 18:25:32 -0600

    Major edits to distance travelled
    Including an option for working with teh RTENO in BBS data.

--- R/calculate_distanceTravelled.R
+++ R/calculate_distanceTravelled.R
@@ -7,33 +7,37 @@
 #' @export
 #'
 calculate_distanceTravelled <- function(dataIn, derivs = T, print = T) {
-    distances <- dataIn %>%
-        # Need to group by variable
-        group_by(variable) %>%
-        # Calculate the Euclidean distance for each variable between two time points.
-        arrange(variable, time) %>%
-        # Calculates first difference
-        mutate(dx = value - lag(value)) %>%
-        # Remove the first dx for each variable because it == NA
-        na.omit(dx) %>%
-        # Remove the grouping factor
-        ungroup() %>%
-        group_by(time) %>%
-        # Sum the dx across all species at each time point
-        summarize(ds = sqrt(sum(dx ^ 2))) %>%
-        # Calculate cumulative sum of ds
-        mutate(s = cumsum(ds))
 
-    if (derivs == T) {
-        distances <- distances %>%
-            mutate(dsdt = ((s - lag(s)) / (time - lag(time))),
-                   d2sdt2 = ((dsdt - lag(dsdt)) / (time - lag(time)))) %>%
-            ungroup()
-
-    } # Calculate the derivatives of the cumulative sum (s)
-
-    if(print == T){head(distances)} # Print df to screen
-
-    return(distances)
-
+    
+    # Calculate distance traveled with special option for using BBS data identifier,
+{
+  if ('RTENO' %in% colnames(dataIn)) {
+    distances <- dataIn %>% group_by(variable) %>% arrange(variable,
+                                                           time) %>% mutate(dx = value - lag(value)) %>% na.omit(dx) %>%
+      ungroup() %>% group_by(time, RTENO) %>% summarise(ds = sqrt(sum(dx ^ 2))) %>%
+      ungroup() %>%
+      mutate(s = cumsum(ds))
+  }
+  if (!('RTENO' %in% colnames(dataIn))) {
+    distances <- dataIn %>% group_by(variable) %>% arrange(variable,
+                                                           time) %>% mutate(dx = value - lag(value)) %>% na.omit(dx) %>%
+      ungroup() %>% group_by(time, RTENO) %>% summarise(ds = sqrt(sum(dx ^
+                                                                        2))) %>%
+      ungroup() %>%
+      mutate(s = cumsum(ds))
+  }
 }
+
+# Add the derivatives if desired
+if (derivs == T) {
+  distances <- distances %>% mutate(dsdt = ((s - lag(s)) / (time -
+                                                              lag(time))),
+                                    d2sdt2 = ((dsdt - lag(dsdt)) / (time -
+                                                                      lag(time)))) %>% ungroup()
+}
+if (print == T) {
+  head(distances)
+}
+return(distances)
+}
+
