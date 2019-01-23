af39054ab3730dc1c9c74c3a2e71852f2e4b3dad
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-01-23 11:56:14 -0600

    revert next loop change.
    did not work

--- R/rdm_window_analysis.R
+++ R/rdm_window_analysis.R
@@ -54,11 +54,9 @@ rdm_window_analysis <- function(dataIn,
     # Leave loop if not enough data points
     if (length(unique(winData$time)) < min.window.dat | nrow(winData) <= min.window.dat) {
       warning("Fewer than min.window.dat time points -- need more to calculate metrics. Skipping window.")
-     
-    } 
+      next
+    }
     # Calcuate the metrics if in argument to.calc
-   if (!(length(unique(winData$time)) < min.window.dat | nrow(winData) <= min.window.dat)) {
-        
     if ("FI" %in% to.calc) {
       FI_temp = NULL
       FI_temp <- calculate_FisherInformation(dataInFI = winData, fi.equation = fi.equation) %>%
@@ -69,7 +67,7 @@ rdm_window_analysis <- function(dataIn,
 
       FI <- rbind(FI_temp, FI)
 
-      } # close FI if
+      }
     if ("VI" %in% to.calc) {
       VI_temp = NULL
       VI_temp <- calculate_VI(winData,  fill = fill) %>%
@@ -80,13 +78,12 @@ rdm_window_analysis <- function(dataIn,
 
       VI <- rbind(VI_temp, VI)
 
-    } # close VI if
+    }
     if ("EWS" %in% to.calc) {
       EWS <- calculate_EWS(winData) %>% rbind(EWS)
-    } # end EWS if
-     } # end min.samp.size if
+    }
 
-  }  # close nWin looping
+  }  # End nWin loops
 
 
 # Create new df -----------------------------------------------------------
