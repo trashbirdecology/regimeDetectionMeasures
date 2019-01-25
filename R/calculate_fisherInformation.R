0e669d040ffa5ce0d32b60f3c795ab464aa0f795
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-01-25 13:10:51 -0600

    ADD CELL ID
    Added cell ID to the function to make compatible with bbsRDM.
    
    I expect the example to no longer function properly with the ex. data in this package, because  it does not have cell ID.

--- R/calculate_fisherInformation.R
+++ R/calculate_fisherInformation.R
@@ -5,57 +5,53 @@
 #' @param min.window.dat
 #' @export
 #'
-calculate_FisherInformation <- function(dataInFI, min.window.dat =2,  fi.equation =  "7.12") {
-    
-  FItemp <- NULL  
-    
-  if (!("7.12" %in% fi.equation | "7.3b" %in% fi.equation |
-        "7.3c" %in% fi.equation)) {
-    warning("Unrecognized equation supplied (fi.equation must be one of c(7.3b, 7.3c, 7.12)")
-  }
-  
-  if (!("dsdt" %in% colnames(dataInFI))) {
-    dataInFI <- calculate_distanceTravelled(dataInFI, derivs = T)
-  }
-  
-  require(kedd)
-  require(caTools)
-  
-  
-  dataInFI <- dataInFI %>% mutate(TT = max(time) - min(time),
-                                  p = (1 / TT) * (1 / dsdt)) %>%  filter(ds != 0)
-  
-  if (fi.equation == "7.3b" & nrow(dataInFI) > min.window.dat) {
-    p <- dataInFI$p
-    s <- dataInFI$s
-    dp <- lead(p) - p
-    ds <- lead(s) - s
-    dpds <- dp / ds
-    ind <- 1:(length(s) - 1)
-    FItemp <- trapz(s[ind], (1 / p[ind]) * dpds[ind] ^ 2)
-  }
-  if (fi.equation == "7.3c" & nrow(dataInFI) > min.window.dat) {
-    q <- sqrt(dataInFI$p)
-    s <- dataInFI$s
-    dq <- lead(q) - q
-    ds <- lead(s) - s
-    dqds <- dq / ds
-    ind <- 1:(length(s) - 1)
-    FItemp <- 4 * trapz(s[ind], dqds[ind] ^ 2)
-  }
-   if (fi.equation == "7.12" & nrow(dataInFI %>% na.omit(dsdt)) > min.window.dat) {
- 
-   
-    # Because this equation divides by dsdt, dsdt != 0
-    dataInFI <- dataInFI %>% na.omit(dsdt)
-    
-    t <- dataInFI$time
-    s <- dataInFI$s
-    TT <- max(t) - min(t)
-    dsdt <- dataInFI$dsdt
-    d2sdt2 <- dataInFI$d2sdt2
-    ind <- 1:(length(s) - 1)
-    FItemp <- (1 / TT) * trapz(t[ind], d2sdt2 ^ 2 / dsdt ^ 4)
-  }
-  return(FItemp)
+calculate_FisherInformation <- function(dataInFI, min.window.dat =2,  fi.equation =  "7.12"){
+    FItemp <- NULL
+    if (!("7.12" %in% fi.equation | "7.3b" %in% fi.equation |
+          "7.3c" %in% fi.equation)) {
+        warning("Unrecognized equation supplied (fi.equation must be one of c(7.3b, 7.3c, 7.12)")
+    }
+    if (!("dsdt" %in% colnames(dataInFI))) {
+        dataInFI <- calculate_distanceTravelled(dataInFI, derivs = T)
+    }
+    require(kedd)
+    require(caTools)
+
+    dataInFI <- dataInFI %>% mutate(TT = max(time) - min(time),
+                                    p = (1/TT) * (1/dsdt)) %>% filter(ds != 0)
+
+
+    if (fi.equation == "7.3b" & nrow(dataInFI) > min.window.dat) {
+        p <- dataInFI$p
+        s <- dataInFI$s
+        dp <- lead(p) - p
+        ds <- lead(s) - s
+        dpds <- dp/ds
+        ind <- 1:(length(s) - 1)
+        FItemp <- trapz(s[ind], (1/p[ind]) * dpds[ind]^2)
+    }
+    if (fi.equation == "7.3c" & nrow(dataInFI) > min.window.dat) {
+        q <- sqrt(dataInFI$p)
+        s <- dataInFI$s
+        dq <- lead(q) - q
+        ds <- lead(s) - s
+        dqds <- dq/ds
+        ind <- 1:(length(s) - 1)
+        FItemp <- 4 * trapz(s[ind], dqds[ind]^2)
+    }
+    if (fi.equation == "7.12" & nrow(dataInFI %>% na.omit(dsdt)) >
+        min.window.dat) {
+        dataInFI <- dataInFI %>% na.omit(dsdt)
+        t <- dataInFI$time
+        s <- dataInFI$s
+        TT <- max(t) - min(t)
+        dsdt <- dataInFI$dsdt
+        d2sdt2 <- dataInFI$d2sdt2
+        ind <- 1:(length(s) - 1)
+        FItemp <- (1/TT) * trapz(t[ind], d2sdt2^2/dsdt^4)
+    }
+
+    FItemp <- cbind(FItemp, min(dataInFI$cellID) , min(dataInFI$cellID))
+    names(FItemp) <- c('FIvalue', 'cellID_min', 'cellID_max' )
+    return(FItemp)
 }
