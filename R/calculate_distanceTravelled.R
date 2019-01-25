55e9a899d97ca0c766e44ac706aefc6bf3c219ca
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-01-25 13:12:44 -0600

    Add CELL ID to the function.
    The example data does not contain cell ID, so I need to add one.

--- R/calculate_distanceTravelled.R
+++ R/calculate_distanceTravelled.R
@@ -1,37 +1,44 @@
-#' @title Calculate the `distance travelled` by the entire system. Also calculates the velocity and acceleration of the entire system over the time series.
+#' Calculate the `distance travelled` by the entire system.
 #'
-#' @param dataIn
-#' @param derivs
-#' @param print
+#' Also calculates the velocity and acceleration of the entire system over the time series.
+#'
+#' @param dataIn input dataframe
+#' @param derivs logical (default TRUE). calculate derivatives as well
+#' @param print logical (default TRUE). print output
 #'
 #' @export
 #'
-calculate_distanceTravelled <- function(dataIn, derivs = T, print = T) {  
-    
-    
-    distances <- dataIn %>% group_by(variable) %>% arrange(variable,
-                                                       time) %>% mutate(dx = value - lag(value)) %>% na.omit(dx) %>%
-    ungroup() %>% 
-    group_by(time) %>% 
-    mutate(ds = sqrt(sum(dx ^ 2))) %>%
-    ungroup() %>% 
-    distinct(time,  ds) %>% 
-    mutate(s = cumsum(ds)) %>% 
-        ungroup()
+calculate_distanceTravelled <-
+    function(dataInDist,
+             derivs = T,
+             print = T) {
+
+        distances <- dataInDist %>% group_by(variable) %>% arrange(variable,
+                                                                   time) %>% mutate(dx = value - lag(value)) %>% na.omit(dx) %>%
+            ungroup() %>%
+            group_by(time) %>%
+            mutate(ds = sqrt(sum(dx ^ 2))) %>%
+            ungroup() %>%
+            distinct(time,  ds,.keep_all=T) %>%
+            dplyr::select(cellID, time, ds) %>%
+            arrange(time) %>%
+            mutate(s = cumsum(ds)) %>%
+            ungroup()
 
 
 
-if (derivs == T) {
-    distances <- distances %>% mutate(dsdt = ((s - lag(s)) / (time -
-                                                                  lag(time))), d2sdt2 = ((dsdt - lag(dsdt)) /
-                                                                                             (time - lag(time)))) %>% ungroup()
-}
-if (print == T) {
-    head(distances)
-}
+        if (derivs == T) {
+            distances <- distances %>% mutate(dsdt = ((s - lag(s)) / (time -
+                                                                          lag(time))), d2sdt2 = ((dsdt - lag(dsdt)) /
+                                                                                                     (time - lag(time)))) %>% ungroup()
+        }
+        if (print == T) {
+            head(distances)
+        }
 
-distances<-as.data.frame(distances) # being buggy with group by and gather. keep for now. 
-    
-return(distances)
-    
+        distances <-
+            as.data.frame(distances) # being buggy with group by and gather. keep for now.
+
+        return(distances)
+
     }
